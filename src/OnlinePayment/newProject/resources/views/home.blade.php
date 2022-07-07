82da1f2d24672bc09bd8919c6818bf1c179336bb
Gorjan Spirovski <54940861+Gorjan2000@users.noreply.github.com>
2022-07-07 21:20:43 +0200

    Update home.blade.php

--- src/OnlinePayment/newProject/resources/views/home.blade.php
+++ src/OnlinePayment/newProject/resources/views/home.blade.php
@@ -24,3 +24,36 @@
 
 @endsection
 
+
+@section('body')
+    <div class="" style="background-image: url('{{asset('images/home-bg.jpg')}}'); height: 100%;">
+        <div class="text-center mx-auto font-bold text-white align-items-center row" style="height: 100%; background-color: rgba(15,16,17,0.8)">
+            <div>
+                <h1 class="display-1" style="font-family: sans-serif">e-INVOICE</h1>
+                <h4>Welcome to 22th century, Welcome to Digitalization!</h4>
+            </div>
+
+        </div>
+
+    </div>
+@endsection
+
+
+@section('JsSection')
+    <script src="{{asset('vendor/jquery/jquery-3.2.1.min.js')}}"></script>
+    <!--===============================================================================================-->
+    <script src="{{asset('vendor/animsition/js/animsition.min.js')}}"></script>
+    <!--===============================================================================================-->
+    <script src="{{asset('vendor/bootstrap/js/popper.js')}}"></script>
+    <script src="{{asset('vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+    <!--===============================================================================================-->
+    <script src="{{asset('vendor/select2/select2.min.js')}}"></script>
+    <!--===============================================================================================-->
+    <script src="{{asset('vendor/daterangepicker/moment.min.js')}}"></script>
+    <script src="{{asset('vendor/daterangepicker/daterangepicker.js')}}"></script>
+    <!--===============================================================================================-->
+    <script src="{{asset('vendor/countdowntime/countdowntime.js')}}"></script>
+    <!--===============================================================================================-->
+    <script src="{{asset('js/main.js')}}"></script>
+
+@endsection
